13aef36253ef30448558a8f78089a5603647b584
nhan <tuannhanle@gmail.com>
2020-03-02 14:32:58 +0700

    update API

--- app.js
+++ app.js
@@ -43,8 +43,8 @@ app.use(cors());
 
 // custom
 app.use('/survey', surverRouter);
-app.use(express.static(path.join(__dirname, 'build')));
 
+app.use(express.static(path.join(__dirname, 'build')));
 
 app.get('/*', (req, res) => {
   res.sendFile(path.join(__dirname, 'build', 'index.html'));
