6bf451d3fdff68887d807cd0245d3f761fbfabba
nhan <tuannhanle@gmail.com>
2020-03-19 14:27:27 +0700

    update client

--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.7f9221a0d990c4ada08f1961876f30e8.js"
+  "/precache-manifest.af6ce6c590449db9bbd1fc27790a1b95.js"
 );
 
 self.addEventListener('message', (event) => {
